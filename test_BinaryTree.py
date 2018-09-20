1f0e7f61240315d121527be335fc2d01c1a890a9
sastaffo <sastaffo@tcd.ie>
2018-09-20 12:02:31 +0100

    adding test function headers

--- test_BinaryTree.py
+++ test_BinaryTree.py
@@ -1,2 +1,19 @@
 """
 """
+import unittest
+
+class TestNode(unittest.TestCase):
+    def test_is_parent(self):
+
+    def test_add_left_child(self):
+
+    def test_add_right_child(self):
+
+
+
+class TestBinaryTree(unittest.TestCase):
+    def test_init(self):
+
+    def test_contains(self):
+
+    def test_find_LCA(self):
