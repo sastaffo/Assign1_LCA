bbe97e87719506bf25a50bd3afe5f9a5c0d56584
sastaffo <sastaffo@tcd.ie>
2018-09-21 11:15:32 +0100

    adding function END comments to functions

--- BinaryTree.py
+++ BinaryTree.py
@@ -2,12 +2,15 @@ class BinaryTree:
 
     def ___init___(self, head):
         self.head = head
+	# END ___init___
 
     def contains(self, node):
         # checks if node is in the tree, returns boolean
+	# END contains
 
     def find_LCA(self, node1, node2):
         # finds the LCA of the 2 nodes passed, returns node that is LCA
+	# END find_LCA
 
 
 
@@ -17,11 +20,14 @@ class BinaryTree:
 class Node:
     def ___init___(self):
         self.is_parent = False
+	# END ___init___
 
     def add_left_child(self, left_child):
         self.left_child = left_child
         self.is_parent = True
+	# END add_left_child
 
     def add_right_child(self, right_child):
         self.right_child = right_child
         self.is_parent = True
+	# END add_right_child
--- test_BinaryTree.py
+++ test_BinaryTree.py
@@ -1,19 +1,35 @@
 """
 """
+from BinaryTree import BinaryTree
+from BinaryTree import Node
 import unittest
 
 class TestNode(unittest.TestCase):
+	def setUp(self):
+		# sets up object to be tested
+	# END setUp
+
     def test_is_parent(self):
+	# END test_is_parent
 
     def test_add_left_child(self):
+	# END test_add_left_child
 
     def test_add_right_child(self):
+	# END test_add_right_child
 
 
 
 class TestBinaryTree(unittest.TestCase):
+	def setUp(self):
+		# sets up object to be tested
+	# END setUp
+
     def test_init(self):
+	# END test_init
 
     def test_contains(self):
+	# END test_contains
 
     def test_find_LCA(self):
+	# END test_find_LCA
