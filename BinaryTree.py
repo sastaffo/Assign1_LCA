93c9b46bcdd20aadf62a59e77d8261a410f6ec5f
sastaffo <sastaffo@tcd.ie>
2018-09-22 14:48:40 +0100

    changing 'value' var to 'key' var

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,25 +1,7 @@
-class BinaryTree:
-
-    def ___init___(self, head):
-        self.head = head
-	# END ___init___
-
-    def contains(self, node):
-        # checks if node is in the tree, returns boolean
-	# END contains
-
-    def find_LCA(self, node1, node2):
-        # finds the LCA of the 2 nodes passed, returns node that is LCA
-	# END find_LCA
-
-
-
-
-
 
 class Node:
-    def ___init___(self, value):
-		self.value = value
+    def ___init___(self, key):
+		self.key = key
         self.is_parent = False
 	# END ___init___
 
@@ -32,3 +14,19 @@ class Node:
         self.right_child = right_child
         self.is_parent = True
 	# END add_right_child
+# END Node
+
+class BinaryTree:
+
+    def ___init___(self, head):
+        self.head = head
+	# END ___init___
+
+    def contains(self, key):
+        # checks if node is in the tree, returns boolean
+	# END contains
+
+    def find_LCA(self, node1, node2):
+        # finds the LCA of the 2 nodes passed, returns node that is LCA
+	# END find_LCA
+# END BinaryTree
