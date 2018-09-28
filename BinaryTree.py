57d0d94a37ede3fa58c1edf3e217fd722a9138a3
sastaffo <sastaffo@tcd.ie>
2018-09-28 14:04:54 +0100

    adding lowest common ancestor function from stack overflow

--- BinaryTree.py
+++ BinaryTree.py
@@ -64,9 +64,41 @@ class BinaryTree:
 		return None
 	# END __contains_rec
 
-	def find_LCA(self, node1, node2):
+	# code adapted from https://stackoverflow.com/a/46454780
+	def find_LCA(self, root, p, q):
 		# finds the LCA of the 2 nodes passed, returns node that is LCA
-		b1 = self.contains(node1)
-		b2 = self.contains(node2)
+		flag=0
+		if root is None: return None
+		if p.key == root.key or q.key == root.key:
+			flag=1
+			#return root
+
+		left_subtree = self.find_LCA(root.left_child, p, q)
+		right_subtree = self.find_LCA(root.right_child, p, q)
+
+		if left_subtree is None and right_subtree is None:
+			if flag==0: return None
+			return root
+
+		if left_subtree is not None and right_subtree is not None:
+			return root
+
+		if left_subtree is None:
+			if flag==1:
+				if ((right_subtree.value!=p and right_subtree.value!=q) or
+						right_subtree.value==root.value):
+					return right_subtree
+				if right_subtree.value!=root.value:
+					return root
+				return right_subtree
+		# else right_subtree is None
+		if flag==1:
+			if ((left_subtree.key!=p and left_subtree.value!=q) or
+					left_subtree.key==root.key):
+				return left_subtree
+			if left_subtree.value!=root.value:
+				return root
+		# else flag != 1
+		return left_subtree
 	# END find_LCA
 # END BinaryTree
