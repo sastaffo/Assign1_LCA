9b960fc78f4b911bee3ba438b633aace4cc1a781
sastaffo <sastaffo@tcd.ie>
2018-10-14 18:20:18 +0100

    changing test and class files to fix some failing tests

--- DirectedGraph.py
+++ DirectedGraph.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 class Node:
 	def __init__(self, key):
 		self.key = key
@@ -10,9 +10,9 @@ class Node:
 		return self.key == node.key
 	# END equals
 
-	def __add_edge_to(self, node):
+	def add_edge_to(self, node):
 		self.has_edge_to.append(node)
-	# END __add_edge_from
+	# END __add_edge_to
 # END Node
 
 class DirectedGraph:
@@ -32,23 +32,30 @@ class DirectedGraph:
 
 	# TODO
 	def __compareEdges(self, digraph):
-		return False
+		for from_node, to_node in self.edges & digraph.edges:
+			if (not self.edges[from_node].equals(digraph[from_node])
+					and not self.edges[to_node].equals(digraph[to_node])):
+				return False
+			# END if
+		# END for
+		return True
+	# END __compareEdges
 
 	def add_node(self, node):
-		if none is not None:
+		if node is not None:
 			self.nodes.append(node)
 		# END if
 	# END add_node
 
-	def add_edge_key(self, to_key, from_key):
-		self.add_edge(Node(to_key), Node(from_key))
+	def add_edge_key(self, from_key, to_key):
+		self.add_edge(Node(from_key), Node(to_key))
 
-	def add_edge(self, to, from):
-		if to is None or from is None: return
-		if self.contains(to) and self.contains(from):
-			self.edges[from] = to
-			from.__add_edge_to(to)
-			if self.source is None or self.source.equals(to):
+	def add_edge(self, from_node, to_node):
+		if to_node is None or from_node is None: return
+		if self.contains(to_node) and self.contains(from_node):
+			self.edges[from_node] = to_node
+			from_node.add_edge_to(to_node)
+			if self.source is None or self.source.equals(to_node):
 				self.set_source
 			# END if
 		# END if
@@ -57,8 +64,8 @@ class DirectedGraph:
 	def set_source(self):
 		for x in self.nodes:
 			source_found = True
-			for from,to in self.edges:
-				if to.equals(x): source_found = False
+			for from_node, to_node in self.edges:
+				if to_node.equals(x): source_found = False
 			if source_found:
 				self.source = x
 				return
@@ -68,21 +75,22 @@ class DirectedGraph:
 
 	def contains(self, goal_node):
 		# checks if node with passed key is in the tree, returns boolean
+		# returns True if None is passed when nodes list is empty
+		if goal_node is None:
+			return len(self.nodes)==0
 		for x in self.nodes:
-			if x.equals(goal_node): return x
+			if x.equals(goal_node): return True
 		# END for
-		return None
+		return False
 	# END contains
 
 	def find_LCA(self, p, q):
-		p_found = self.contains(p)
-		q_found = self.contains(q)
-		if p_found is None or q_found is None:
+		if not self.contains(p) or not self.contains(q):
 			return None
 		path_p = self.shortest_path(p)
 		path_q = self.shortest_path(q)
-		for i in range(0,len(path_p)):
-			if !path_p[i].equals(path_q[i]):
+		for i in range(1,len(path_p)):
+			if not path_p[i].equals(path_q[i]):
 				return path_p[i-1]
 	# END find_LCA
 
@@ -90,12 +98,12 @@ class DirectedGraph:
 	# https://gist.github.com/mdsrosa/c71339cb23bc51e711d8
 	def shortest_path(self, dest):
 		paths = self.generate_paths()
-		path = []
+		path = deque()
 		path.append(self.source)
-		_dest = paths[dest]
-		while !dest_p.equals(self.source):
+		_dest = paths[] # TODO fix indexing
+		while not dest_p.equals(self.source):
 			path.append(_dest)
-			_dest = paths[_dest]
+			_dest = paths[] # TODO fix indexing
 		# END while
 		path.append(dest)
 		return path
@@ -118,16 +126,15 @@ class DirectedGraph:
 						min_node = node
 					# END if
 			# END for
+			if min_node is None: break
+
+			tmp_nodes.remove(min_node)
+			for edge in graph.edges[min_node]:
+				if edge not in visited:
+					paths[edge] = min_node
+				# END if
+			# END for
 		# END while
-		if min_node is None:
-			break
-		# END if
-		tmp_nodes.remove(min_node)
-		for edge in graph.edges[min_node]:
-			if edge not in visited:
-				paths[edge] = min_node
-			# END if
-		# END for
-	return paths
+		return paths
 	# END generate_paths
 # END BinaryTree
--- test_DirectedGraph.py
+++ test_DirectedGraph.py
@@ -47,7 +47,7 @@ class TestDirectedGraph(unittest.TestCase):
 
 	def test_equals(self):
 		self.assertFalse(self.digraph.equals(None))
-		self.assertFalse(self.digraph.equals(self.digraph2))
+		self.assertFalse(self.digraph.equals(self.digraph_building))
 		# TODO add more tests here
 
 		self.assertTrue(self.digraph_building.equals(DirectedGraph()))
@@ -60,11 +60,13 @@ class TestDirectedGraph(unittest.TestCase):
 		# add two nodes
 		self.digraph_building.add_node(Node('A'))
 		self.digraph_building.add_node(Node('B'))
-		self.assertEqual(self.digraph_building.nodes, [Node('A'), Node('B')])
+		self.assertTrue(compare_nodes_list(
+				self.digraph_building.nodes, [Node('A'), Node('B')]))
 
 		# add null node
 		self.digraph_building.add_node(None)
-		self.assertEqual(self.digraph_building.nodes, [Node('A'), Node('B')])
+		self.assertTrue(compare_nodes_list(
+				self.digraph_building.nodes, [Node('A'), Node('B')]))
 	# END test_add_node
 
 	def test_add_edge(self):
@@ -72,41 +74,48 @@ class TestDirectedGraph(unittest.TestCase):
 		self.digraph_building.add_node(Node('A'))
 		self.digraph_building.add_node(Node('B'))
 		self.digraph_building.add_edge(Node('A'),Node('B'))
-		self.assertEqual(self.digraph_building.edges, {Node('A'): Node('B')})
+		self.assertTrue(compare_edges_dict(
+				self.digraph_building.edges, {Node('A'): Node('B')}))
 
 		# add existing edge: should not change edge map
-		expected_edges = digraph.edges
+		expected_edges = self.digraph.edges
 		# check equality before making changes
 		self.digraph.add_edge(Node('A'), Node('B'))
-		self.assertEqual(self.digraph.edges, expected_edges)
+		self.assertTrue(compare_edges_dict(
+				self.digraph.edges, expected_edges))
 
 		# add edge to None: should not change edge map
 		self.digraph.add_edge(Node('C'), None)
-		self.assertEqual(self.digraph.edges, expected_edges)
+		self.assertTrue(compare_edges_dict(
+				self.digraph.edges, expected_edges))
 
 		# add edge to invalid node: should not change edge map
 		self.digraph.add_edge(Node('C'), Node('G'))
-		self.assertEqual(self.digraph.edges, expected_edges)
+		self.assertTrue(compare_edges_dict(
+				self.digraph.edges, expected_edges))
 	# END test_add_edge
 
 	def test_add_edge_key(self):
 		self.digraph_building.add_node(Node('A'))
 		self.digraph_building.add_node(Node('B'))
 		self.digraph_building.add_edge_key('A','B')
-		self.assertEqual(self.digraph_building.edges, {Node('A'): Node('B')})
+		self.assertTrue(compare_edges_dict(
+				self.digraph_building.edges, {Node('A'): Node('B')}))
 		self.digraph_building.add_node(Node('B'))
 
 	def test_contains(self):
-		 # test for valid
-		self.assertTrue(self.digraph_building.contains(Node('A')))
-		# test for non-valid
+		# test for none
+		self.assertTrue(self.digraph_building.contains(None))
+		# test for invalid
 		self.assertFalse(self.digraph_building.contains(Node('D')))
 
+		# test for None
+		self.assertFalse(self.digraph.contains(None))
 		# test for valid
 		self.assertTrue(self.digraph.contains(Node('A')))
 		self.assertTrue(self.digraph.contains(Node('D')))
 		# test for invalid
-		self.assertFalse(self.digraph.contains(Nodes('F')))
+		self.assertFalse(self.digraph.contains(Node('F')))
 	# END test_contains
 
 	def test_find_LCA(self):
@@ -129,4 +138,21 @@ class TestDirectedGraph(unittest.TestCase):
 		self.assertTrue(Node('C').equals(
 				self.digraph.find_LCA(Node('C'), Node('C'))))
 	# END test_find_LCA_same
+
 # END TestBinaryTree
+
+def compare_nodes_list(a, b):
+	for x in a & b:
+		if not (a[x].equals(b[x])): return False
+	# END for
+	return True
+# END compare_nodes_list
+
+def compare_edges_dict(a_dict, b_dict):
+	a = a_dict.items()
+	b = b_dict.items()
+	for x in a & b:
+		if ((not a[x[0]].equals(b[x[0]]))
+				and (not a[x[1]].equals(b[x[1]]))):
+			return False
+# END compare_edges_dict
