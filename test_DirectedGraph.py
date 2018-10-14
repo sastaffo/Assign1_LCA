aaa1e6fcb40e29c54bca26ebfe945c1c001926cf
sastaffo <sastaffo@tcd.ie>
2018-10-14 15:13:02 +0100

    finishing writing tests for directed graphs

--- test_DirectedGraph.py
+++ test_DirectedGraph.py
@@ -29,10 +29,10 @@ class TestDirectedGraph(unittest.TestCase):
 		self.digraph_building = DirectedGraph()
 		# {empty}
 
-		self.digraph1 = DirectedGraph()
-		self.digraph1.nodes = [	Node('A'),  Node('B'),  Node('C'),
+		self.digraph = DirectedGraph()
+		self.digraph.nodes = [	Node('A'),  Node('B'),  Node('C'),
 								Node('D'),  Node('E') ]
-		self.digraph1.edges = {	Node('A') : Node('B'),
+		self.digraph.edges = {	Node('A') : Node('B'),
 								Node('A') : Node('C'),
 								Node('B') : Node('D'),
 								Node('B') : Node('E'),
@@ -42,22 +42,12 @@ class TestDirectedGraph(unittest.TestCase):
 		#      'B'   'C'
 		#     /  \  /
 		#  'D'   'E'
-
-		self.digraph2 = DirectedGraph()
-		self.digraph2.nodes = [	Node('A'), Node('B'), Node('C'), Node('D') ]
-		self.digraph2.edges = { Node('A') : Node('B'),
-								Node('A') : Node('C'),
-								Node('B') : Node('C'),
-								Node('C') : Node('D')}
-		#     'A'
-		#    /  \
-		# 'B' -- 'C' -- 'D'
 	# END setUp
 
 
 	def test_equals(self):
-		self.assertFalse(self.digraph1.equals(None))
-		self.assertFalse(self.digraph1.equals(self.digraph2))
+		self.assertFalse(self.digraph.equals(None))
+		self.assertFalse(self.digraph.equals(self.digraph2))
 		# TODO add more tests here
 
 		self.assertTrue(self.digraph_building.equals(DirectedGraph()))
@@ -85,24 +75,18 @@ class TestDirectedGraph(unittest.TestCase):
 		self.assertEqual(self.digraph_building.edges, {Node('A'): Node('B')})
 
 		# add existing edge: should not change edge map
-		expected_edges = {	Node('A') : Node('B'),
-							Node('A') : Node('C'),
-							Node('B') : Node('D'),
-							Node('B') : Node('E'),
-							Node('C') : Node('E')
-						 }
+		expected_edges = digraph.edges
 		# check equality before making changes
-		self.assertEqual(self.digraph1.edges, expected_edges)
-		self.digraph1.add_edge(Node('A'), Node('B'))
-		self.assertEqual(self.digraph1.edges, expected_edges)
+		self.digraph.add_edge(Node('A'), Node('B'))
+		self.assertEqual(self.digraph.edges, expected_edges)
 
 		# add edge to None: should not change edge map
-		self.digraph1.add_edge(Node('C'), None)
-		self.assertEqual(self.digraph1.edges, expected_edges)
+		self.digraph.add_edge(Node('C'), None)
+		self.assertEqual(self.digraph.edges, expected_edges)
 
 		# add edge to invalid node: should not change edge map
-		self.digraph1.add_edge(Node('C'), Node('G'))
-		self.assertEqual(self.digraph1.edges, expected_edges)
+		self.digraph.add_edge(Node('C'), Node('G'))
+		self.assertEqual(self.digraph.edges, expected_edges)
 	# END test_add_edge
 
 	def test_add_edge_key(self):
@@ -113,37 +97,36 @@ class TestDirectedGraph(unittest.TestCase):
 		self.digraph_building.add_node(Node('B'))
 
 	def test_contains(self):
-		 # test for source
+		 # test for valid
 		self.assertTrue(self.digraph_building.contains(Node('A')))
 		# test for non-valid
 		self.assertFalse(self.digraph_building.contains(Node('D')))
 
-		# test for source
-		self.assertTrue(self.digraph1.contains(Node('A')))
-
-
+		# test for valid
+		self.assertTrue(self.digraph.contains(Node('A')))
+		self.assertTrue(self.digraph.contains(Node('D')))
+		# test for invalid
+		self.assertFalse(self.digraph.contains(Nodes('F')))
 	# END test_contains
 
 	def test_find_LCA(self):
-		self.assertTrue(self.binary_tree.head.equals(
-				self.binary_tree.find_LCA(Node('D'), Node('C'))))
 		self.assertTrue(Node('B').equals(
-				self.binary_tree.find_LCA(Node('D'), Node('E'))))
+				self.digraph.find_LCA(Node('D'), Node('E'))))
+		self.assertTrue(Node('A').equals(
+				self.digraph.find_LCA(Node('B')), Node('C')))
 	# END test_find_LCA
 
 	def test_find_LCA_invalid(self):
-		self.assertFalse(self.binary_tree.find_LCA(Node('B'), Node('F')))
-		#with self.assertRaises(exceptions.TypeError):
-			#do something
+		self.assertFalse(self.digraph.find_LCA(Node('B'), Node('F')))
 	# END test_find_LCA_invalid
 
 	def test_find_LCA_self(self):
 		self.assertTrue(Node('B').equals(
-				self.binary_tree.find_LCA(Node('B'), Node('D'))))
+				self.digraph.find_LCA(Node('B'), Node('D'))))
 	# END test_find_LCA_self
 
 	def test_find_LCA_same(self):
 		self.assertTrue(Node('C').equals(
-				self.binary_tree.find_LCA(Node('C'), Node('C'))))
+				self.digraph.find_LCA(Node('C'), Node('C'))))
 	# END test_find_LCA_same
 # END TestBinaryTree
