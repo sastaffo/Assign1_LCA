6fd34c696c135a8ade919a05a9ac0f404ef18758
sastaffo <sastaffo@tcd.ie>
2018-10-14 16:35:00 +0100

    adding lowest common ancestor functionality to directed graph class

--- DirectedGraph.py
+++ DirectedGraph.py
@@ -17,6 +17,7 @@ class Node:
 
 class DirectedGraph:
 	def __init__(self):
+		self.source = None # Any node that has no edges TO it
 		self.nodes = []
 		self.edges = {}
 	# END __init__
@@ -47,101 +48,69 @@ class DirectedGraph:
 		if self.contains(to) and self.contains(from):
 			self.edges[from] = to
 			from.__add_edge_to(to)
+			if self.source is None or self.source.equals(to):
+				self.set_source
+			# END if
 		# END if
 	# END add_edge
 
+	def set_source(self):
+		for x in self.nodes:
+			source_found = True
+			for from,to in self.edges:
+				if to.equals(x): source_found = False
+			if source_found:
+				self.source = x
+				return
+		# END for
+		# if no new source found, graph is cyclic
+		raise ValueError('Graph has no source, graph is cyclic.')
+
 	def contains(self, goal_node):
 		# checks if node with passed key is in the tree, returns boolean
-		return self.__contains_rec(goal_node, self.source)
-	# END contains
-
-	def __contains_rec(self, goal_node, currentNode):
-		#recursively finds the node with the passed key
-		if currentNode is None: return None
-		if goal_node.equals(currentNode): return currentNode
-
-		if len(currentNode.edges_from)>0:
-			found_node = self.__contains_rec(goal_node, currentNode.left_child)
-			if found_node is None:
-				found_node = self.__contains_rec(
-								goal_node, currentNode.right_child)
-			# END if
-			return found_node
-		# END if
-		return None
-	# END __contains_rec
-
-	def __contains_rec(self, current, goal, path=[]):
-		path = path + [start]
-		if current.equals(goal):
-			return path
-		for node in self.nodes:
-			if node not in path:
-				newpath = find_path(graph, node, end, path)
-				if newpath: return newpath
+		for x in self.nodes:
+			if x.equals(goal_node): return x
 		# END for
 		return None
-	# END __contains_rec
+	# END contains
 
 	def find_LCA(self, p, q):
 		p_found = self.contains(p)
 		q_found = self.contains(q)
 		if p_found is None or q_found is None:
 			return None
-		return self.__find_LCA(self.head, p, q)
+		path_p = self.shortest_path(p)
+		path_q = self.shortest_path(q)
+		for i in range(0,len(path_p)):
+			if !path_p[i].equals(path_q[i]):
+				return path_p[i-1]
 	# END find_LCA
 
-	# code adapted from https://stackoverflow.com/a/46454780
-	def __find_LCA(self, root, p, q):
-		# finds the LCA of the 2 nodes passed, returns node that is LCA
-		if root is None: return None
-		flag=0
-		if p.key == root.key or q.key == root.key:
-			flag=1
-			#indicates later to return root
-		# END if
-
-		left = self.__find_LCA(root.left_child, p, q)
-		right = self.__find_LCA(root.right_child, p, q)
-
-		if left is None and right is None:
-			if flag==0: return None
-			# else flag == 1
-			return root
-
-		if left is not None and right is not None:
-			return root
-
-		if left is None:
-			if flag==1:
-				if ((not right.equals(p) and not right.equals(q)) or
-						right.equals(root)):
-					return right
-				if not right.equals(root):
-					return root
-			# else flag != 1
-			return right
-
-		# else right is None:
-		if flag==1:
-			if ((not left.equals(p) and not left.equals(q)) or
-					left.equals(root)):
-				return left
-			if not left.equals(root):
-				return root
-		# else flag != 1
-		return left
-	# END __find_LCA
+	# based on code from
+	# https://gist.github.com/mdsrosa/c71339cb23bc51e711d8
+	def shortest_path(self, dest):
+		paths = self.generate_paths()
+		path = []
+		path.append(self.source)
+		_dest = paths[dest]
+		while !dest_p.equals(self.source):
+			path.append(_dest)
+			_dest = paths[_dest]
+		# END while
+		path.append(dest)
+		return path
+	# END shortest_path
 
 	# based on dijkstra's algorithm in python
 	# https://gist.github.com/econchick/4666413
-	def __contains_rec(self, goal_node):
+	def generate_paths(self):
 		visited = {self.source: 0}
-		path = {}
+		paths = {}
+		tmp_nodes = self.nodes
 
-		while nodes:
+		while tmp_nodes:
 			min_node = None
-			for node in nodes:
+			for node in tmp_nodes:
 				if node in visited:
 					if min_node is None:
 						min_node = node
@@ -150,20 +119,15 @@ class DirectedGraph:
 					# END if
 			# END for
 		# END while
-
 		if min_node is None:
 			break
 		# END if
-		nodes.remove(min_node)
-		current_weight = visited[min_node]
-
+		tmp_nodes.remove(min_node)
 		for edge in graph.edges[min_node]:
-			weight = current_weight + graph.distance[(min_node, edge)]
-			if edge not in visited or weight < visited[edge]:
-				visited[edge] = weight
-				path[edge] = min_node
+			if edge not in visited:
+				paths[edge] = min_node
 			# END if
 		# END for
-	return visited, path
-	# ENd LCA_dijsktra
+	return paths
+	# END generate_paths
 # END BinaryTree
